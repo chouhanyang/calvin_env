4774f97cc5a80bb63b7ea1b8c879df91eedccbc5
Chou-han Yang <chouyang@tesla.com>
2023-11-03 17:13:28 -0400

    Add urdfpy into tacto

--- tacto/sensor.py
+++ tacto/sensor.py
@@ -13,7 +13,7 @@ import cv2
 import numpy as np
 import pybullet as p
 import trimesh
-from urdfpy import URDF
+from tacto.urdfpy import URDF
 
 from .renderer_mp import Renderer
 # from .renderer import Renderer
